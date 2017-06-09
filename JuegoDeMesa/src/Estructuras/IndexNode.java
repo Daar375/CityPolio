40d7d923c282a4cbe84619ddf0adeaef5e3e91db
Daar375 <Daar375@gmail.com>
2017-06-09 14:39:13 -0600

    Add files via upload

--- JuegoDeMesa/src/Estructuras/IndexNode.java
+++ JuegoDeMesa/src/Estructuras/IndexNode.java
@@ -3,25 +3,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 
-public class IndexNode<K extends Comparable<K>, T> extends Node<K,T> {
+public class IndexNode<K extends Comparable<K>, T> extends TreeNode<K,T> {
 
 	// m nodes
-	protected ArrayList<Node<K,T>> children; // m+1 children
+	protected ArrayList<TreeNode<K,T>> children; // m+1 children
 
-	public IndexNode(K key, Node<K,T> child0, Node<K,T> child1) {
+	public IndexNode(K key, TreeNode<K,T> child0, TreeNode<K,T> child1) {
 		isLeafNode = false;
 		keys = new ArrayList<K>();
 		keys.add(key);
-		children = new ArrayList<Node<K,T>>();
+		children = new ArrayList<TreeNode<K,T>>();
 		children.add(child0);
 		children.add(child1);
 	}
 
-	public IndexNode(List<K> newKeys, List<Node<K,T>> newChildren) {
+	public IndexNode(List<K> newKeys, List<TreeNode<K,T>> newChildren) {
 		isLeafNode = false;
 
 		keys = new ArrayList<K>(newKeys);
-		children = new ArrayList<Node<K,T>>(newChildren);
+		children = new ArrayList<TreeNode<K,T>>(newChildren);
 
 	}
 
@@ -32,9 +32,9 @@ public class IndexNode<K extends Comparable<K>, T> extends Node<K,T> {
 	 * @param e
 	 * @param index
 	 */
-	public void insertSorted(Entry<K, Node<K,T>> e, int index) {
+	public void insertSorted(Entry<K, TreeNode<K,T>> e, int index) {
 		K key = e.getKey();
-		Node<K,T> child = e.getValue();
+		TreeNode<K,T> child = e.getValue();
 		if (index >= keys.size()) {
 			keys.add(key);
 			children.add(child);
