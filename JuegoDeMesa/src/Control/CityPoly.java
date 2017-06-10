d4c0df5381e22994657289d9ed9a9502c36f3868
Malabaryan <malabaryan@hotmail.com>
2017-06-10 12:40:30 -0600

    Update CityPoly.java
    Fix Ñ

--- JuegoDeMesa/src/Control/CityPoly.java
+++ JuegoDeMesa/src/Control/CityPoly.java
@@ -1,8 +1,6 @@
 package Control;
 
-import java.util.ArrayList;
-
-import Deck.DeckCiudades;
+import Deck.DeckCuidades;
 import Deck.DeckRetos;
 import Deck.Reto;
 import Estructuras.BPlusTree;
@@ -14,9 +12,9 @@ public class CityPoly {
 	private BPlusTree<String, Jugador> Players = new BPlusTree();
 	private Jugador player1;
 	private Jugador player2;
-	private DeckCiudades DecC= new DeckCiudades();
+	private DeckCuidades DecC= new DeckCuidades();
 	private DeckRetos DecR= new DeckRetos();
-	
+
 	
 	public CityPoly(){
 		bluidDecks();
@@ -24,7 +22,7 @@ public class CityPoly {
 	public void newPlayer(String Name, String Pass) {
 		if (Players.search(Name) == null) {
 			Jugador player = new Jugador();
-			player.setContraseña(Pass);
+			player.setContrasenha(Pass);
 			player.setName(Name);
 			player.setPoints(0);
 			Players.insert(Name, player);
@@ -34,13 +32,9 @@ public class CityPoly {
 	public void bluidDecks(){
 		GeoMap tool = new GeoMap();
 		HTTPPlaces get = new HTTPPlaces();
-		ArrayList City = new ArrayList();
-		City.addAll(tool.loadplaces(get.getplaces("9.9354028", "-84.0753903", "restaurant"), Type.restaurant));
-
-		City.addAll(tool.loadplaces(get.getplaces("9.9354028", "-84.0753903", "hospital"), Type.hospital));
-		City.addAll(tool.loadplaces(get.getplaces("9.9354028", "-84.0753903", "church"), Type.church));
-		DecC.AddAll(City);
-
+		DecC.AddAll(tool.loadplaces(get.post("9.9354028", "-84.0753903", "restaurant"), Type.restaurant));
+		DecC.AddAll(tool.loadplaces(get.post("9.9354028", "-84.0753903", "hospital"), Type.hospital));
+		DecC.AddAll(tool.loadplaces(get.post("9.9354028", "-84.0753903", "church"), Type.church));
 		int index = 0;
 		while(index!=30){
 			Reto reto= new Reto();
@@ -55,7 +49,7 @@ public class CityPoly {
 	public void logIn(String Name, String Pass) {
 		Jugador playerfound = Players.search(Name);
 		if (playerfound != null) {
-			if (playerfound.getContraseña() == Pass) {
+			if (playerfound.getContrasenha() == Pass) {
 				if (player1 == null) {
 					player1 = playerfound;
 				} else {
