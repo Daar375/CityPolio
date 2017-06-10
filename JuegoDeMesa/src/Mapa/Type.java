ff794d8cd0684c5bca1c187bafd797b2d36696c1
Malabaryan <malabaryan@hotmail.com>
2017-06-09 23:47:32 -0600

    Modify Type.java
    Se agregaron tipos

--- JuegoDeMesa/src/Mapa/Type.java
+++ JuegoDeMesa/src/Mapa/Type.java
@@ -1,5 +1,5 @@
 package Mapa;
 
 public enum Type {
-	restaurant,church,school,park,hospital
+	restaurant,church,school,park,hospital,stadium,school,police,museum,bank,library,hospital,gym,lodging
 }
