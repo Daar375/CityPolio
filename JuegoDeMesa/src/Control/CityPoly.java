0c27405462fa2fc4b8c1aefca0885e50b3d9ce91
Daar375 <Daar375@gmail.com>
2017-06-10 12:59:24 -0600

    Update CityPoly.java

--- JuegoDeMesa/src/Control/CityPoly.java
+++ JuegoDeMesa/src/Control/CityPoly.java
@@ -24,7 +24,7 @@ public class CityPoly {
 	public void newPlayer(String Name, String Pass) {
 		if (Players.search(Name) == null) {
 			Jugador player = new Jugador();
-			player.setContraseña(Pass);
+			player.setContrasenha(Pass);
 			player.setName(Name);
 			player.setPoints(0);
 			Players.insert(Name, player);
@@ -55,7 +55,7 @@ public class CityPoly {
 	public void logIn(String Name, String Pass) {
 		Jugador playerfound = Players.search(Name);
 		if (playerfound != null) {
-			if (playerfound.getContraseña() == Pass) {
+			if (playerfound.getContraseÃ±a() == Pass) {
 				if (player1 == null) {
 					player1 = playerfound;
 				} else {
