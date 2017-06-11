933b6b73b7e618ee586d8f3a42772482022aa6b1
Malabaryan <malabaryan@hotmail.com>
2017-06-10 22:39:02 -0600

    Progra Actualizada
    Muchos cambios en codigo, clases eliminadas, CityPolio actualizado, etc

--- JuegoDeMesa/src/Control/CityPoly.java
+++ JuegoDeMesa/src/Control/CityPoly.java
@@ -3,11 +3,15 @@ package Control;
 import Deck.DeckCiudades;
 import Deck.DeckRetos;
 import Deck.Reto;
+import Deck.*;
+import Estructuras.*;
 import Estructuras.BPlusTree;
 import Google.GeoMap;
 import Google.HTTPPlaces;
+import Mapa.Place;
 import Mapa.Type;
 import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class CityPoly {
 	private BPlusTree<String, Jugador> Players = new BPlusTree();
@@ -15,10 +19,12 @@ public class CityPoly {
 	private Jugador player2;
 	private DeckCiudades DecC= new DeckCiudades();
 	private DeckRetos DecR= new DeckRetos();
+        private Graph Grafo;
 
 	
 	public CityPoly(){
 		bluidDecks();
+                
 	}
 	public void newPlayer(String Name, String Pass) {
 		if (Players.search(Name) == null) {
@@ -35,8 +41,8 @@ public class CityPoly {
                 HTTPPlaces get = new HTTPPlaces();
 		ArrayList City = new ArrayList();
                 
-		Double Long = -84.0753903;
-                Double Lat = 9.9354028;
+		Double Long = -0.118092;
+                Double Lat = 51.509865;
                 for(Type t : Type.values()){
                     City.addAll(tool.loadplaces(HTTPPlaces.getplaces(Lat.toString(),Long.toString() , t.toString()), t));
                 }
@@ -50,9 +56,88 @@ public class CityPoly {
 			DecR.Add(reto);
 			index++;
 		}
-		System.out.println("hoa");
+		//System.out.println("hoa");
 
 	}
+        
+        public void iniciarJuego(){
+            int ran = genRandom(0,DecC.getCartas().size());
+            System.out.println(DecC.getCartas().size());
+            System.out.println(ran);
+//            Ciudad CiudadJuego = DecC.getCartas().get(ran);
+                            
+            Ciudad CiudadPrueba = new Ciudad();
+            CiudadPrueba.setLatitud(51.509865);
+            CiudadPrueba.setLongitud(-0.118092);
+            CiudadPrueba.setName("London");
+            
+            
+            int i = 0;
+            ArrayList<Place> po = new ArrayList();
+            for(i = 0;i < 1000;i+=60){
+                Place p = new Place();
+                p.setLatitud(CiudadPrueba.getLatitud()+i/genRandom(10,50));
+                p.setLongitud(CiudadPrueba.getLongitud()+i/genRandom(10,50));
+                p.setName("Nombre");
+                p.setTipo(Type.park);
+                p.setValor(5);
+                po.add(p);
+                System.out.println("i vale: " + i);
+                
+               
+            }
+            CiudadPrueba.setPlaces(po);
+            
+            
+            Grafo = new Graph(CiudadPrueba.getPlaces().size());
+            genGrafo(CiudadPrueba);
+            
+        }
+        
+        public void genGrafo(Ciudad CiudadJuego){
+            int indexP = -1;        // Primer indice para recorrer los places
+            int indexP2 = -1;       // Segundo indice para recorrer los places
+            double distanciaEntrePlace = 0;
+            boolean ExistenAristas = false;
+            Place P;
+            Place P2;
+            int distanciaPermitida = 50;
+            
+            while(indexP < CiudadJuego.getPlaces().size() - 1){
+                indexP++;
+                indexP2 = -1;
+                P = CiudadJuego.getPlaces().get(indexP);
+                ExistenAristas = false;
+                while(indexP2 < CiudadJuego.getPlaces().size() - 1){
+                    indexP2++;
+                    P2 = CiudadJuego.getPlaces().get(indexP2);
+                    distanciaEntrePlace = GeoMap.distance(P.getLatitud(),P.getLongitud(),P2.getLatitud(),P2.getLongitud()); 
+                    System.out.println(distanciaEntrePlace + " es la distancia entre el vertice " + indexP + " y " + indexP2);
+                    if(P == P2){
+                        ;
+                    }
+                    else if(distanciaEntrePlace < distanciaPermitida){
+                        
+                        Grafo.addEdge(indexP, indexP2, (int)distanciaEntrePlace, false);
+                        ExistenAristas = true;
+                    }
+                    
+                }
+                // Si busco y no encontro places cerca para agregarse como vecinos
+                // al grafo, hace la busqueda otra vez pero con 50mts mas
+                if(!ExistenAristas){
+                    distanciaPermitida+=50;
+                    System.out.print("+50 ");
+                    indexP--;}
+                else{distanciaPermitida = 50;}
+            }
+        }
+        
+        private int genRandom(int rangoI, int rangoF){
+            System.out.println("Random generado!");
+            return ThreadLocalRandom.current().nextInt(rangoI, rangoF);
+        }
+        
 	public void logIn(String Name, String Pass) {
 		Jugador playerfound = Players.search(Name);
 		if (playerfound != null) {
@@ -65,10 +150,36 @@ public class CityPoly {
 				}
 			}
 		}
+                
 	}
+
+    public Graph getGrafo() {
+        return Grafo;
+    }
+
+    public void setGrafo(Graph Grafo) {
+        this.Grafo = Grafo;
+    }
+        
+        
 	
 	
 	public static void main(String[] args) throws Exception {
 		CityPoly test = new CityPoly();
+
+                test.DecC.Print();
+                for(Reto c : test.DecR.getCartas()){
+                    System.out.println(c.toString());
+                }
+                
+                test.iniciarJuego();
+                Dijkstra Dij = new Dijkstra(test.getGrafo());
+                Dij.dijkstra(0);
+                System.out.println(Dij.shortestPath(0).size());
+                
+                
+                     
+                
+                
 	}
 }
--- JuegoDeMesa/src/Google/GeoMap.java
+++ JuegoDeMesa/src/Google/GeoMap.java
@@ -49,7 +49,7 @@ public class GeoMap {
 		return lugares;
 	}
 
-	private double distance(double lat1, double lon1, double lat2, double lon2) {
+	public static double distance(double lat1, double lon1, double lat2, double lon2) {
 		double theta = lon1 - lon2;
 		double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))	+ Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.cos(deg2rad(theta));
 		dist = Math.acos(dist);
