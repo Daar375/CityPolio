4ec0aea46cbacbe8a3df249cfa19a93984b5a8bb
Daar375 <Daar375@gmail.com>
2017-06-11 00:10:07 -0600

    Add files via upload

--- JuegoDeMesa/src/Control/CityPoly.java
+++ JuegoDeMesa/src/Control/CityPoly.java
@@ -13,19 +13,19 @@ import Mapa.Type;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
-public class CityPoly {
+public class CityPoly implements IConstants {
 	private BPlusTree<String, Jugador> Players = new BPlusTree();
 	private Jugador player1;
 	private Jugador player2;
-	private DeckCiudades DecC= new DeckCiudades();
-	private DeckRetos DecR= new DeckRetos();
-        private Graph Grafo;
+	private DeckCiudades DecC = new DeckCiudades();
+	private DeckRetos DecR = new DeckRetos();
+	private Graph Grafo;
 
-	
-	public CityPoly(){
+	public CityPoly() {
 		bluidDecks();
-                
+
 	}
+
 	public void newPlayer(String Name, String Pass) {
 		if (Players.search(Name) == null) {
 			Jugador player = new Jugador();
@@ -36,27 +36,34 @@ public class CityPoly {
 		}
 	}
 
-	public void bluidDecks(){
+	public void bluidDecks() {
 		GeoMap tool = new GeoMap();
-                HTTPPlaces get = new HTTPPlaces();
+		HTTPPlaces get = new HTTPPlaces();
 		ArrayList City = new ArrayList();
-                
-		Double Long = -0.118092;
-                Double Lat = 51.509865;
-                Ciudad ciudad = new Ciudad();
-                ciudad.setLatitud(Lat);
-                ciudad.setLatitud(Long);
-                ciudad.setName("London");
-                for(Type t : Type.values()){
-                    City.addAll(tool.loadplaces(HTTPPlaces.getplaces(Lat.toString(),Long.toString() , t.toString()), t));
-                }
-            	ciudad.setPlaces(City);
-
-                DecC.Add(ciudad);
-                DecC.Print();
 		int index = 0;
-		while(index!=30){
-			Reto reto= new Reto();
+
+
+
+		while (index != LISTA_CIUDADES.length) {
+			Ciudad ciudad = new Ciudad();
+			System.out.println(LISTA_CIUDADES[index][2]);
+			ciudad.setLatitud(Double.parseDouble(LISTA_CIUDADES[index][1]));
+			ciudad.setLongitud(Double.parseDouble(LISTA_CIUDADES[index][2]));
+			ciudad.setName(LISTA_CIUDADES[index][0]);
+			for (Type t : Type.values()) {
+				
+				
+	
+				City.addAll(tool.loadplaces(HTTPPlaces.getplaces(LISTA_CIUDADES[index][1], LISTA_CIUDADES[index][2], t.toString()), t));
+			}
+			ciudad.setPlaces(City);
+
+			DecC.Add(ciudad);
+			index++;
+		}
+		index = 0;
+		while (index != 30) {
+			Reto reto = new Reto();
 			reto.setTipo(DecR.randomEnum(Type.class));
 			reto.setCantidad(new Dado().rollDice());
 			DecR.Add(reto);
@@ -64,85 +71,86 @@ public class CityPoly {
 		}
 
 	}
-        
-        public void iniciarJuego(){
-            int ran = genRandom(0,DecC.getCartas().size());
-            System.out.println(DecC.getCartas().size());
-            System.out.println(ran);
-//            Ciudad CiudadJuego = DecC.getCartas().get(ran);
-                            
-            Ciudad CiudadPrueba = new Ciudad();
-            CiudadPrueba.setLatitud(51.509865);
-            CiudadPrueba.setLongitud(-0.118092);
-            CiudadPrueba.setName("London");
-            
-            
-            int i = 0;
-            ArrayList<Place> po = new ArrayList();
-            for(i = 0;i < 1000;i+=60){
-                Place p = new Place();
-                p.setLatitud(CiudadPrueba.getLatitud()+i/genRandom(10,50));
-                p.setLongitud(CiudadPrueba.getLongitud()+i/genRandom(10,50));
-                p.setName("Nombre");
-                p.setTipo(Type.park);
-                p.setValor(5);
-                po.add(p);
-               // System.out.println("i vale: " + i);
-                
-               
-            }
-            CiudadPrueba.setPlaces(po);
-            
-            
-            Grafo = new Graph(CiudadPrueba.getPlaces().size());
-            genGrafo(CiudadPrueba);
-            
-        }
-        
-        public void genGrafo(Ciudad CiudadJuego){
-            int indexP = -1;        // Primer indice para recorrer los places
-            int indexP2 = -1;       // Segundo indice para recorrer los places
-            double distanciaEntrePlace = 0;
-            boolean ExistenAristas = false;
-            Place P;
-            Place P2;
-            int distanciaPermitida = 50;
-            
-            while(indexP < CiudadJuego.getPlaces().size() - 1){
-                indexP++;
-                indexP2 = -1;
-                P = CiudadJuego.getPlaces().get(indexP);
-                ExistenAristas = false;
-                while(indexP2 < CiudadJuego.getPlaces().size() - 1){
-                    indexP2++;
-                    P2 = CiudadJuego.getPlaces().get(indexP2);
-                    distanciaEntrePlace = GeoMap.distance(P.getLatitud(),P.getLongitud(),P2.getLatitud(),P2.getLongitud()); 
-                    System.out.println(distanciaEntrePlace + " es la distancia entre el vertice " + indexP + " y " + indexP2);
-                    if(P == P2){
-                        ;
-                    }
-                    else if(distanciaEntrePlace < distanciaPermitida){
-                        
-                        Grafo.addEdge(indexP, indexP2, (int)distanciaEntrePlace, false);
-                        ExistenAristas = true;
-                    }
-                    
-                }
-                // Si busco y no encontro places cerca para agregarse como vecinos
-                // al grafo, hace la busqueda otra vez pero con 50mts mas
-                if(!ExistenAristas){
-                    distanciaPermitida+=50;
-                    System.out.print("+50 ");
-                    indexP--;}
-                else{distanciaPermitida = 50;}
-            }
-        }
-        
-        private int genRandom(int rangoI, int rangoF){
-            System.out.println("Random generado!");
-            return ThreadLocalRandom.current().nextInt(rangoI, rangoF);
-        }
-        
+
+	public void iniciarJuego() {
+		int ran = genRandom(0, DecC.getCartas().size());
+		System.out.println(DecC.getCartas().size());
+		System.out.println(ran);
+		Ciudad CiudadJuego = DecC.getCartas().get(ran);
+
+		/*
+		 * Ciudad CiudadPrueba = new Ciudad();
+		 * /*CiudadPrueba.setLatitud(51.509865);
+		 * CiudadPrueba.setLongitud(-0.118092); CiudadPrueba.setName("London");
+		 */
+
+		int i = 0;
+		ArrayList<Place> po = new ArrayList();
+		for (i = 0; i < 1000; i += 60) {
+			Place p = new Place();
+			p.setLatitud(CiudadJuego.getLatitud() + i / genRandom(10, 50));
+			p.setLongitud(CiudadJuego.getLongitud() + i / genRandom(10, 50));
+			p.setName("Nombre");
+			p.setTipo(Type.park);
+			p.setValor(5);
+			po.add(p);
+			// System.out.println("i vale: " + i);
+
+		}
+		CiudadJuego.setPlaces(po);
+
+		Grafo = new Graph(CiudadJuego.getPlaces().size());
+		genGrafo(CiudadJuego);
+
+	}
+
+	public void genGrafo(Ciudad CiudadJuego) {
+		int indexP = -1; // Primer indice para recorrer los places
+		int indexP2 = -1; // Segundo indice para recorrer los places
+		double distanciaEntrePlace = 0;
+		boolean ExistenAristas = false;
+		Place P;
+		Place P2;
+		int distanciaPermitida = 50;
+
+		while (indexP < CiudadJuego.getPlaces().size() - 1) {
+			indexP++;
+			indexP2 = -1;
+			P = CiudadJuego.getPlaces().get(indexP);
+			ExistenAristas = false;
+			while (indexP2 < CiudadJuego.getPlaces().size() - 1) {
+				indexP2++;
+				P2 = CiudadJuego.getPlaces().get(indexP2);
+				distanciaEntrePlace = GeoMap.distance(P.getLatitud(), P.getLongitud(), P2.getLatitud(),
+						P2.getLongitud());
+				System.out
+						.println(distanciaEntrePlace + " es la distancia entre el vertice " + indexP + " y " + indexP2);
+				if (P == P2) {
+					;
+				} else if (distanciaEntrePlace < distanciaPermitida) {
+
+					Grafo.addEdge(indexP, indexP2, (int) distanciaEntrePlace, false);
+					ExistenAristas = true;
+				}
+
+			}
+			// Si busco y no encontro places cerca para agregarse como vecinos
+			// al grafo, hace la busqueda otra vez pero con 50mts mas
+			if (!ExistenAristas) {
+				distanciaPermitida += 50;
+				System.out.print("+50 ");
+				indexP--;
+			} else {
+				distanciaPermitida = 50;
+			}
+		}
+	}
+
+	private int genRandom(int rangoI, int rangoF) {
+		System.out.println("Random generado!");
+		return ThreadLocalRandom.current().nextInt(rangoI, rangoF);
+	}
+
 	public void logIn(String Name, String Pass) {
 		Jugador playerfound = Players.search(Name);
 		if (playerfound != null) {
@@ -155,36 +163,29 @@ public class CityPoly {
 				}
 			}
 		}
-                
+
 	}
 
-    public Graph getGrafo() {
-        return Grafo;
-    }
+	public Graph getGrafo() {
+		return Grafo;
+	}
 
-    public void setGrafo(Graph Grafo) {
-        this.Grafo = Grafo;
-    }
-        
-        
-	
-	
+	public void setGrafo(Graph Grafo) {
+		this.Grafo = Grafo;
+	}
+
 	public static void main(String[] args) throws Exception {
 		CityPoly test = new CityPoly();
 
-                test.DecC.Print();
-                for(Reto c : test.DecR.getCartas()){
-                    System.out.println(c.toString());
-                }
-                
-                test.iniciarJuego();
-                Dijkstra Dij = new Dijkstra(test.getGrafo());
-                Dij.dijkstra(0);
-                System.out.println(Dij.shortestPath(0).size());
-                
-                
-                     
-                
-                
+		test.DecC.Print();
+		for (Reto c : test.DecR.getCartas()) {
+			System.out.println(c.toString());
+		}
+
+		test.iniciarJuego();
+		Dijkstra Dij = new Dijkstra(test.getGrafo());
+		Dij.dijkstra(0);
+		System.out.println(Dij.shortestPath(0).size());
+
 	}
 }
--- JuegoDeMesa/src/Control/IConstants.java
+++ JuegoDeMesa/src/Control/IConstants.java
@@ -8,5 +8,6 @@ public interface IConstants {
 
 	public final File RANKINGFILE = new File("E//ranking");
 	public final String MCS_IDKEY = "AIzaSyCbb9-AtcNbXpRLRWaR-fuqDha0SEoss6k";
-
+	public final String[][] LISTA_CIUDADES = {{"San Jose Morazan","9.9346199","-84.0752503"}		
+	};
 }
